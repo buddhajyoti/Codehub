b606abc3416944eb242529856e938ca40a1f169c
buddhajyoti <buddhajyoti.c@tcs.com>
2014-06-19 18:59:02 +0530

    commiting

---
+++ FirstGitProject/src/test/MyFirstGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author Buddha
+ *
+ */
+public class MyFirstGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Git Hello World");
+
+	}
+
+}
